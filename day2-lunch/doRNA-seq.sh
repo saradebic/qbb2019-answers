2e0c141165e21c5a82f7d7bebe4429b8d612351c
saradebic <sara@debic.org>
2019-08-27 12:33:16 -0400

    added bash code to run RNAseq

---
+++ day2-lunch/doRNA-seq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GENOME=~/data/genomes/BDGP6.fa
+ANNOTATION=~/data/genomes/BDGP6.Ensembl.81.gtf
+THREADS=4
+
+for SAMPLE in SRR072893 SRR072903 SRR072905 SRR072915
+do
+  echo "*** Processing $SAMPLE"
+  cp ~/data/rawdata/$SAMPLE.fastq .
+  fastqc $SAMPLE.fastq
+  hisat2 -p $THREADS -x BDGP6 -U $SAMPLE.fastq -S stdout.sam
+  samtools sort -@ $THREADS stdout.sam > stdout.bam
+  samtools index stdout.bam
+  stringtie stdout.bam -G $ANNOTATION -o $SAMPLE_alignment -e -B -p 4
+done
