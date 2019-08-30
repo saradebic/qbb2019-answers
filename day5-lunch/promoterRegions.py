0f69d8a9414c5b77ca4b0a4ce5492ba4f2b61a06
saradebic <sara@debic.org>
2019-08-30 13:19:42 -0400

    fixed code

--- day5-lunch/promoterRegions.py
+++ day5-lunch/promoterRegions.py
@@ -11,21 +11,27 @@ import statsmodels.api as sm
 ctab = pd.read_csv(sys.argv[1], sep="\t")
 all_start_end_values = ctab.loc[:,["chr", "t_name", "strand", "start", "end"]]
 
-print("Chr" + "\t" + "t_name" + "\t" + "promoter start" + "\t" + "promoter end")
 for index, row in all_start_end_values.iterrows():
     if row.loc["strand"] == "+":
         promoterStart = int(row.loc["start"])
         promoter_start_plus_500 = promoterStart + 500
         promoter_start_minus_500 = promoterStart - 500
-        print(str(row.loc["chr"]) + "\t" + str(row.loc["t_name"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500))
+        if promoter_start_plus_500 <= 0:
+            promoter_start_plus_500 = 1
+        elif promoter_start_minus_500 <= 0:
+            promoter_start_minus_500 = 1
+        print(str(row.loc["chr"]) + "\t" + str(promoter_start_minus_500) + "\t" + str(promoter_start_plus_500) + "\t" + str(row.loc["t_name"]))
     elif row.loc["strand"] == "-":    
         promoterStart = int(row.loc["end"])
         promoter_start_plus_500 = promoterStart + 500
         promoter_start_minus_500 = promoterStart - 500
-        print(str(row.loc["chr"]) + "\t" + str(row.loc["t_name"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500))
+        if promoter_start_plus_500 < 0:
+            promoter_start_plus_500 = 1
+        elif promoter_start_minus_500 < 0:
+            promoter_start_minus_500 = 1
+        print(str(row.loc["chr"]) + "\t" + str(promoter_start_minus_500) + "\t" + str( promoter_start_plus_500) + "\t" + str(row.loc["t_name"]))
     
         
-# promoter_start_values = all_start_end_values.drop(columns=["start", "end"])
-# print(promoter_start_values)
+
 
 
