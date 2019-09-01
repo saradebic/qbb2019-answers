f80a0981ae45265e5be1837fbd44201855cb31af
saradebic <sara@debic.org>
2019-09-01 19:46:40 -0400

    All remaining code for day 5

---
+++ day5-lunch/LogMultipleLinearRegression.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""Usage: ./LogMultipleLinearRegression.py <t_data.ctab> <H3K4me1_tab> <H3K4me3_tab> <H3K9me3_tab>""" #outtab is a file with histone data
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import scipy
+
+ctab = pd.read_csv(sys.argv[1], sep="\t", index_col = "t_name")
+H3K4me1_tab = pd.read_csv(sys.argv[2], sep="\t", header=None, names=["t_name", "size", "covered", "sum", "mean0", "mean"],index_col = "t_name")
+H3K4me3_tab = pd.read_csv(sys.argv[3], sep="\t", header=None, names=["t_name", "size", "covered", "sum", "mean0", "mean"], index_col = "t_name")
+H3K9me3_tab = pd.read_csv(sys.argv[4], sep="\t", header=None, names=["t_name", "size", "covered", "sum", "mean0", "mean"], index_col = "t_name")
+
+df = pd.DataFrame(np.log(ctab['FPKM']+1))
+df["H3K4me1"] = H3K4me1_tab["mean"]
+df["H3K4me3"] = H3K4me3_tab["mean"]
+df["H3K9me3"] = H3K9me3_tab["mean"]
+
+model = sm.formula.ols(formula="FPKM ~ H3K4me1 + H3K4me3 + H3K9me3", data= df)
+ols_results = model.fit()
+
+print(ols_results.summary())
+
+fig, ax = plt.subplots()
+ax.hist(ols_results.resid, bins=10000, range=(-100,100))
+ax.set_xlim((-10,10))
+plt.xlabel("Residual values")
+plt.ylabel("Frequency")
+fig.savefig("LogResiduals.png")
+plt.close(fig)
---
+++ day5-lunch/multipleLinearRegression.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""Usage: ./multipleLinearRegression.py <t_data.ctab> <H3K4me1_tab> <H3K4me3_tab> <H3K9me3_tab>""" #outtab is a file with histone data
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import scipy
+
+ctab = pd.read_csv(sys.argv[1], sep="\t", index_col = "t_name")
+H3K4me1_tab = pd.read_csv(sys.argv[2], sep="\t", header=None, names=["t_name", "size", "covered", "sum", "mean0", "mean"],index_col = "t_name")
+H3K4me3_tab = pd.read_csv(sys.argv[3], sep="\t", header=None, names=["t_name", "size", "covered", "sum", "mean0", "mean"], index_col = "t_name")
+H3K9me3_tab = pd.read_csv(sys.argv[4], sep="\t", header=None, names=["t_name", "size", "covered", "sum", "mean0", "mean"], index_col = "t_name")
+
+df = pd.DataFrame(ctab['FPKM'])
+df["H3K4me1"] = H3K4me1_tab["mean"]
+df["H3K4me3"] = H3K4me3_tab["mean"]
+df["H3K9me3"] = H3K9me3_tab["mean"]
+
+model = sm.formula.ols(formula="FPKM ~ H3K4me1 + H3K4me3 + H3K9me3", data= df)
+ols_results = model.fit()
+
+print(ols_results.summary())
+
+
+
+
+
